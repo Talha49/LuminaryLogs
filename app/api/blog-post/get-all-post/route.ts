49f1bfd4caa6c8345f2cdfc629d240d1d962b101
Talha Ghauri <113139113+Talha49@users.noreply.github.com>
2024-03-21 00:32:19 +0500

    update route.ts

--- app/api/blog-post/get-all-post/route.ts
+++ app/api/blog-post/get-all-post/route.ts
@@ -1,26 +1,27 @@
 import prisma from "@/database";
-import { NextRequest, NextResponse } from "next/server";
+import {  NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
-    try {
-        const getAllBlogPost = await prisma.pOST.findMany();
-        if (getAllBlogPost && getAllBlogPost.length) {
-            return NextResponse.json({
-                success: true,
-                data: getAllBlogPost,
-            });
-        } else {
-            return NextResponse.json({
-                success: false,
-                message: "Failed to get",
-            });
-        }
-    } catch (error) {
-        console.error(error);
+export async function GET() {
+  try {
+    const getAllBlogPosts = await prisma.pOST.findMany();
 
-        return NextResponse.json({
-            success: false,
-            message: "Something went wrong!",
-        });
+    if (getAllBlogPosts && getAllBlogPosts.length) {
+      return NextResponse.json({
+        success: true,
+        data: getAllBlogPosts,
+      });
+    } else {
+      return NextResponse.json({
+        success: false,
+        message: "Failed to fetch blog posts. Please try again",
+      });
     }
+  } catch (e) {
+    console.log(e);
+
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong ! Please try again",
+    });
+  }
 }
